9c6b7755d804e040b381673edefb28699b22de01
Your Name <yas.2892@gmail.com>
2019-11-23 17:41:40 -0800

    moved api lab

---
+++ apis_lab/secrets.sh
@@ -0,0 +1,7 @@
+# Secrets for your Eventbrite app
+# Find these at: https://www.eventbrite.ca/account-settings/apps
+# This file should never go into github!
+
+export EVENTBRITE_KEY="NSIPDESH2DW5V5IZIL"
+export EVENTBRITE_SECRET="S7KFTX77UR3KK2UW37SIZGSL323O6N36UKA45DEHST2F425JN2"
+export EVENTBRITE_TOKEN="7KOIM5YQXYZ67UX4CEUQ"
