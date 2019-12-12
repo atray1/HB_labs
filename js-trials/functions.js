88fcbe8f474ec252f0280e2ef9cff60e40fccfc7
Yasmeen Atra <yas.2892@gmail.com>
2019-12-12 16:41:55 +0000

    JavaScript Trials

--- js-trials/functions.js
+++ js-trials/functions.js
@@ -3,14 +3,44 @@
 
 /** 1. isHometown */
 
-// Define your function here
+function isHometown(town) {
+    if (town === 'San Francisco') {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
 
-** 2. getFullName */
+/** 2. getFullName */
 
-// Define your function here
+function getFullName(fname, lname) {
+    
+    return fname + ' ' + lname
+}
 
 
 /** 3. calculateTotal */
 
-// Define your function here
+function calculateTotal(basePrice, state, tax = 0.05){
+    const total = basePrice * (1 + tax);
+    let fee = 0
+    if (state === 'CA') {
+        fee = 0.03 * total;
+    }
+    else if (state === 'PA') {
+        fee = 2;
+    }
+    else if (state === 'MA') {
+        if (basePrice <= 100) {
+            fee = 1
+        }
+        else {
+            fee = 3
+        }
+    }
+
+    return total + fee
+
+}
--- js-trials/objects.js
+++ js-trials/objects.js
@@ -3,11 +3,31 @@
 
 /** 1. countWords */
 function countWords(phrase) {
-	// Replace this with your code
+    const wordCount = {};
+
+    for (const word of phrase.split(' ')) {
+        if (wordCount[word]) {
+            wordCount[word] += 1;
+        }
+        else {
+            wordCount[word] = 1;}
+    }
+    return wordCount
 }
 
 
 /** 2. getMelonsAtPrice */
 function getMelonsAtPrice(price) {
-	// Replace this with your code
+    const melonPrices = {
+        2.50: ['Canteloupe', 'Honeydew'],
+        2.95: ['Watermelon'],
+        3.25: ['Musk', 'Crenshaw'],
+        14.25: ['Christmas']
+    }
+    if (!melonPrices[price]){
+        return
+    }
+    else {
+        return melonPrices[price].sort()
+    }
 }
